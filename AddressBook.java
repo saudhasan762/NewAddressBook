ba7c09b36ece258562638b321d3de7c962aed803
saudhasan762 <azanali152@gmail.com>
2021-02-27 17:04:24 +0530

    Implemented to sort the contacts using name,city,state,zip

--- AddressBook.java
+++ AddressBook.java
@@ -7,6 +7,7 @@ public class AddressBook extends AddressBookOperate {
         System.out.println("! Welcome to the Address Book System !");
         System.out.println("List of Address books: ");
         Map<String, ArrayList<Contact>> map = new HashMap<>();
+
         ArrayList<Contact> list1 = new ArrayList<>();
         ArrayList<Contact> list2 = new ArrayList<>();
         map.put("AddressBook1", list1);
@@ -63,98 +64,5 @@ public class AddressBook extends AddressBookOperate {
             }
         }
     }
-
-    public static void details(ArrayList<Contact> list) {
-        mainlist = list;
-        AddressBook Book = new AddressBook();
-        s: while(true) {
-            int choice = Book.chooseOption();
-            s2: while(true) {
-                if(choice == 1) {
-                    int numOfPerson = 0;
-                    System.out.println("Enter number of Persons to add");
-                    numOfPerson = new Scanner(System.in).nextInt();
-                    for(int i =1;i <=numOfPerson;i++) {
-                        System.out.println("Enter the First name to check");
-                        String checkname = new Scanner(System.in).nextLine();
-                        if(!Book.add(checkname))
-                            break s2;
-                        System.out.println("Enter the First name for Person "+i);
-                        String first = new Scanner(System.in).nextLine();
-                        System.out.println("Enter last name for Person "+i);
-                        String last = new Scanner(System.in).nextLine();
-                        System.out.println("Enter Address for Person "+i);
-                        String address = new Scanner(System.in).nextLine();
-                        System.out.println("Enter city for person "+i);
-                        String city= new Scanner(System.in).nextLine();
-                        System.out.println("Enter state for person "+i);
-                        String state = new Scanner(System.in).nextLine();
-                        System.out.println("Enter zip for Person "+i);
-                        String zip = new Scanner(System.in).nextLine();
-                        System.out.println("Enter Phone number for Person "+i);
-                        String phoneNumber = new Scanner(System.in).nextLine();
-                        System.out.println("Enter email for Person "+i);
-                        String email = new Scanner(System.in).nextLine();
-                        Contact con = new Contact(first,last,address,city,state,zip,phoneNumber,email);
-                        mainlist.add(con);
-                        System.out.println("Added Successfully");
-                    }
-                    break;
-                }
-                else if(choice == 2){
-                    Book.view();
-                    break;
-                }
-                else if(choice == 3) {
-                    System.out.println("Enter first name: ");
-                    String first = new Scanner(System.in).nextLine();
-                    System.out.println("Enter last name: ");
-                    String last = new Scanner(System.in).nextLine();
-                    System.out.println("Enter Address: ");
-                    String address = new Scanner(System.in).nextLine();
-                    System.out.println("Enter city: ");
-                    String city = new Scanner(System.in).nextLine();
-                    System.out.println("Enter state");
-                    String state = new Scanner(System.in).nextLine();
-                    System.out.println("Enter zip");
-                    String zip = new Scanner(System.in).nextLine();
-                    System.out.println("Enter Phone number :");
-                    String phoneNumber = new Scanner(System.in).nextLine();
-                    System.out.println("Enter email: ");
-                    String email = new Scanner(System.in).nextLine();
-                    if(!Book.edit(first, last, address, city, state, zip, phoneNumber, email))
-                        break;
-                    else
-                        System.out.println("Edited Successfully");
-                    break;
-                }
-                else if(choice == 4) {
-                    System.out.println("Enter first name");
-                    String first = new Scanner(System.in).nextLine();
-                    System.out.println("Enter last name: ");
-                    String last = new Scanner(System.in).nextLine();
-                    if(!Book.delete(first, last)) {
-                        break;
-                    }
-                    break;
-                }
-                else if(choice == 5) {
-                    System.out.println("Enter city name");
-                    String city = new Scanner(System.in).nextLine();
-                    System.out.println("Enter state name: ");
-                    String state = new Scanner(System.in).nextLine();
-                    if(Book.searchPerson(city,state) == false) {
-                        break;
-                    }
-                    break;
-                }
-                else if(choice == 0) {
-                    System.out.println("Return to main");;
-                    break s;
-                }
-                else
-                    System.out.println("Invalid choice");
-            }
-        }
-    }
 }
+
